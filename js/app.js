e0bc59db0bc0dc0773631a23d97d4390a792d9f5
Bondhan Mondal <bondhan2725@gmail.com>
2022-03-03 00:11:24 +0600

    28-7 Verify match and use function to reduce duplicate

--- js/app.js
+++ js/app.js
@@ -22,6 +22,12 @@ document.getElementById('key-pad').addEventListener('click', function(event){
         if(number == 'C'){
             calcInput.value = '';
         }
+        else if(number == '<'){
+            const str = calcInput.value;
+            const newStr = str.slice(0, -1);
+            calcInput.value = newStr;
+
+        }
     }
     else{
         const previousNumber = calcInput.value;
@@ -29,3 +35,20 @@ document.getElementById('key-pad').addEventListener('click', function(event){
         calcInput.value = newNumber;
     }
 });
+
+function verifyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+
+    const successMessage = document.getElementById('notify-success');
+    const failErros = document.getElementById('notify-fail');
+
+    if(pin == typedNumbers){
+        successMessage.style.display = 'block';
+        failErros.style.display = 'none';
+    }
+    else{
+        failErros.style.display = 'block';
+        successMessage.style.display = 'none';
+    }
+}
